089b79fcf172c7249642c0d66f5e4d5baf4a85e1
Hlib Dmytriiev <action.dmitrieff@gmail.com>
2024-05-02 13:47:26 +0300

    Search querying implemented with both regular and favorited boards

--- convex/board.ts
+++ convex/board.ts
@@ -51,7 +51,18 @@ export const remove = mutation({
       throw new Error("Unauthorized");
     }
 
-    // TODO: Later check to delete favorite relation as well
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", args.id)
+      )
+      .unique();
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id);
+    }
 
     await ctx.db.delete(args.id);
   },
@@ -105,11 +116,8 @@ export const favorite = mutation({
 
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex("by_user_board_org", (q) =>
-        q
-          .eq("userId", userId)
-          .eq("boardId", board._id)
-          .eq("orgId", args.orgId)
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
 
@@ -146,11 +154,8 @@ export const unfavorite = mutation({
 
     const existingFavorite = await ctx.db
       .query("userFavorites")
-      .withIndex(
-        "by_user_board",
-        (q) =>
-          q.eq("userId", userId).eq("boardId", board._id)
-        // TODO: Check if org_id needed
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", userId).eq("boardId", board._id)
       )
       .unique();
 
