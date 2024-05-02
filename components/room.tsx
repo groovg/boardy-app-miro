1dcb423190832636af1cd7dc55746fa752eb4773
Hlib Dmytriiev <action.dmitrieff@gmail.com>
2024-05-02 14:41:18 +0300

    Connected LiveBlocks and added loading state for room with fallback to skeleton

---
+++ components/room.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ReactNode } from "react";
+import { RoomProvider } from "@/liveblocks.config";
+import { ClientSideSuspense } from "@liveblocks/react";
+import { Divide } from "lucide-react";
+
+interface RoomProps {
+  children: ReactNode;
+  roomId: string;
+  fallback: NonNullable<ReactNode> | null;
+}
+
+export const Room = ({
+  children,
+  roomId,
+  fallback,
+}: RoomProps) => {
+  return (
+    <RoomProvider id={roomId} initialPresence={{}}>
+      <ClientSideSuspense fallback={fallback}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
